d928879fb683b61b017d2de1e625f059959f4e1a
akshara94 <aksharabindhu94@gmail.com>
2023-11-26 21:29:03 +0530

    second commit pgm

---
+++ firsttimetransfer/src/pck/characters.java
@@ -0,0 +1,26 @@
+package pck;
+
+public class characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int mark =60;
+     if (mark >= 90)
+     {
+    	 System.out.println("A grade");
+     
+	}
+     else if (mark >= 80)
+     {
+    	 System.out.println("B Grade");
+    	 }
+     else if (mark>=50)
+     {
+    	 System.out.println("C grade");
+     }
+     else 
+     {
+    	 System.out.println("Fail");
+     }
+}
+}
